7c344eeab974bc6f900872f5d79423de157b4c8c
Wei <171336915@qq.com>
2018-09-06 02:15:25 +0000

    修改和删除收货地址

--- app/Http/Controllers/UserAddressesController.php
+++ app/Http/Controllers/UserAddressesController.php
@@ -8,14 +8,14 @@ use Illuminate\Http\Request;
 
 class UserAddressesController extends Controller
 {
-    //
+    //首页
     public function index(Request $request)
     {
         return view('user_addresses.index', [
             'addresses' => $request->user()->addresses,
         ]);
     }
-
+    //新增收货地址
     public function create()
     {
         return view('user_addresses.create_and_edit', ['address' => new UserAddress()]);
@@ -34,4 +34,36 @@ class UserAddressesController extends Controller
         ]));
         return redirect()->route('user_addresses.index');
     }
+
+    public function update(UserAddress $user_address, UserAddressRequest $request)
+    {
+        $this->authorize('own', $user_address);
+        $user_address->update($request->only([
+            'province',
+            'city',
+            'district',
+            'address',
+            'zip',
+            'contact_name',
+            'contact_phone',
+        ]));
+
+        return redirect()->route('user_addresses.index');
+    }
+
+//    修改地址
+    public function edit(UserAddress $user_address)
+    {
+        $this->authorize('own', $user_address);
+        return view('user_addresses.create_and_edit', ['address' => $user_address]);
+    }
+
+    public function destroy(UserAddress $user_address)
+    {
+        $this->authorize('own', $user_address);
+        $user_address->delete();
+        return [];
+    }
+
+
 }
