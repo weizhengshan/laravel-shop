e01c8e9b964e2fde3d077a66b7a6dd62f40d1706
Wei <171336915@qq.com>
2018-09-07 07:11:37 +0000

    用户界面 - 收藏商品列表

--- routes/web.php
+++ routes/web.php
@@ -14,13 +14,12 @@
 //Route::get('/', 'PagesController@root')->name('root');
 Route::redirect('/', '/products')->name('root');
 Route::get('products', 'ProductsController@index')->name('products.index');
-Route::get('products/{product}', 'ProductsController@show')->name('products.show');
+
 Auth::routes();
 Route::group(['middleware' => 'auth'], function() {
     Route::get('/email_verification/send', 'EmailVerificationController@send')->name('email_verification.send');
     Route::get('/email_verify_notice', 'PagesController@emailVerifyNotice')->name('email_verify_notice');
     Route::get('/email_verification/verify', 'EmailVerificationController@verify')->name('email_verification.verify');
-    // 开始
     Route::group(['middleware' => 'email_verified'], function() {
         Route::get('user_addresses', 'UserAddressesController@index')->name('user_addresses.index');
         Route::get('user_addresses/create', 'UserAddressesController@create')->name('user_addresses.create');
@@ -30,9 +29,9 @@ Route::group(['middleware' => 'auth'], function() {
         Route::delete('user_addresses/{user_address}', 'UserAddressesController@destroy')->name('user_addresses.destroy');
         Route::post('products/{product}/favorite', 'ProductsController@favor')->name('products.favor');
         Route::delete('products/{product}/favorite', 'ProductsController@disfavor')->name('products.disfavor');
-
+        Route::get('products/favorites', 'ProductsController@favorites')->name('products.favorites');
     });
-    // 结束
 });
+Route::get('products/{product}', 'ProductsController@show')->name('products.show');
 
 
