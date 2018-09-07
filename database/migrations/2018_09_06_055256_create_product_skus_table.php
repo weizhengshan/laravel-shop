42a21682825272173069394757a746c021f330f3
Wei <171336915@qq.com>
2018-09-07 05:34:46 +0000

    管理后台 - 新增、编辑商品

--- database/migrations/2018_09_06_055256_create_product_skus_table.php
+++ database/migrations/2018_09_06_055256_create_product_skus_table.php
@@ -20,7 +20,7 @@ class CreateProductSkusTable extends Migration
             $table->decimal('price', 10, 2);
             $table->unsignedInteger('stock');
             $table->unsignedInteger('product_id');
-            $table->foreign('product_id')->references('id')->on('products')->onDelete('cascade');;
+            $table->foreign('product_id')->references('id')->on('products')->onDelete('cascade');
             $table->timestamps();
         });
     }
