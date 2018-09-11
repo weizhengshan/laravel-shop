91b001da3c1cb39a1dbe085cc3de58a1a48dd0d3
Wei <171336915@qq.com>
2018-09-11 05:30:09 +0000

    管理后台 - 订单详情

--- app/Admin/Controllers/OrdersController.php
+++ app/Admin/Controllers/OrdersController.php
@@ -54,4 +54,13 @@ class OrdersController extends Controller
             });
         });
     }
+
+    public function show(Order $order)
+    {
+        return Admin::content(function (Content $content) use ($order) {
+            $content->header('查看订单');
+            // body 方法可以接受 Laravel 的视图作为参数
+            $content->body(view('admin.orders.show', ['order' => $order]));
+        });
+    }
 }
