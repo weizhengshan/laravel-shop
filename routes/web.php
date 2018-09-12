458a03dd910089fa4add8f7290fee2544f9d3a1c
Wei <171336915@qq.com>
2018-09-12 01:35:21 +0000

    用户界面 - 检查优惠券

--- routes/web.php
+++ routes/web.php
@@ -43,12 +43,14 @@ Route::group(['middleware' => 'auth'], function() {
         Route::get('orders/{order}/review', 'OrdersController@review')->name('orders.review.show');
         Route::post('orders/{order}/review', 'OrdersController@sendReview')->name('orders.review.store');
         Route::post('orders/{order}/apply_refund', 'OrdersController@applyRefund')->name('orders.apply_refund');
+        Route::get('coupon_codes/{code}', 'CouponCodesController@show')->name('coupon_codes.show');
     });
 });
 Route::post('payment/alipay/notify', 'PaymentController@alipayNotify')->name('payment.alipay.notify');
 Route::post('payment/wechat/notify', 'PaymentController@wechatNotify')->name('payment.wechat.notify');
 Route::get('products/{product}', 'ProductsController@show')->name('products.show');
 Route::post('payment/wechat/refund_notify', 'PaymentController@wechatRefundNotify')->name('payment.wechat.refund_notify');
+
 //Route::get('alipay', function() {
 //    return app('alipay')->web([
 //        'out_trade_no' => time(),
